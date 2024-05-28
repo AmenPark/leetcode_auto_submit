4f15cd20e11a2c22ce58d4e9b4e591936a4cb3c2
Amen Park <86364207+AmenPark@users.noreply.github.com>
2024-05-28 17:14:21 +0900

    Time: 41 ms (27.18%), Space: 16.5 MB (97.64%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,6 @@
 class Solution:
-    def getsub(self, nums, i, now):
-        if i==len(nums):
-            self.ans.append(now[:])
-            return
-        self.getsub(nums,i+1,now)
-        now.append(nums[i])
-        self.getsub(nums,i+1,now)
-        now.pop()
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.ans = []
-        self.getsub(nums,0,[])
-        return self.ans
+        ans = []
+        for i in range(1<<len(nums)):
+            ans.append([nums[k] for k in range(len(nums)) if (1<<k)&i])
+        return ans
