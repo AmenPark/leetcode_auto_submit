d3be3bdacaa47fa3d76410a045e680c6bad936e5
Amen Park <86364207+AmenPark@users.noreply.github.com>
2024-05-31 12:53:52 +0900

    Time: 69 ms (17.72%), Space: 18.5 MB (76.83%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,6 +1,4 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         n = (w:=reduce(lambda x,y:x^y, nums,0))&-w
-        print(n)
-        ans=reduce(lambda x,y : [x[0]^y if n&y==0 else x[0], x[1]^y if n&y else x[1]] ,nums,[0,0])
-        return ans
+        return reduce(lambda x,y : [x[0]^y if n&y==0 else x[0], x[1]^y if n&y else x[1]] ,nums,[0,0])
