2ed20e86862398511265c09e5070b67e5d7ef9ae
Amen Park <86364207+AmenPark@users.noreply.github.com>
2024-07-19 21:16:17 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
@@ -1,25 +1,29 @@
 class Solution:
     def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
-        rs = [[s,i] for i,s in enumerate(rowSum)]
-        cs = [[s,i] for i,s in enumerate(colSum)]
-        rs.sort()
-        cs.sort()
         N=len(rowSum)
         M=len(colSum)
         ans = [[0 for _ in range(M)] for _ in range(N)]
         i=j=0
+        rss=iter(rowSum)
+        css=iter(colSum)
+        rs=next(rss)
+        cs=next(css)
         while i<N:
-            rsval, rsi = rs[i]
-            csval, csi = cs[j]
-            ans[rsi][csi] = min(rsval,csval)
-            if rsval==csval:
+            if rs==cs:
+                ans[i][j] = colSum[j]
                 i+=1
                 j+=1
-            elif rsval>csval:
-                rs[i][0] -= csval
+                rs=next(rss,-1)
+                cs=next(css,-1)
+            elif rs>cs:
+                ans[i][j] = cs 
+                rs -= cs
                 j+=1
+                cs=next(css,-1)
             else:
-                cs[j][0] -= rsval
+                ans[i][j]=rs
+                cs -= rs
                 i+=1
+                rs=next(rss,-1)
         return ans
                 
