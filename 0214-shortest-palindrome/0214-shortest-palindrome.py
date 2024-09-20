d555b66f18f4f9dc2a433595e2cea9e5c50f53d3
Amen Park <86364207+AmenPark@users.noreply.github.com>
2024-09-20 21:48:24 +0900

    Time: 47 ms (94.7%), Space: 18.2 MB (29.65%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,31 +1,40 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
         fails = [0]
+        def addfails():
+            i=1
+            j=0
+            yield
+            while True:
+                if s[i]==s[j]:
+                    fails.append(j+1)
+                    i+=1
+                    j+=1
+                    yield
+                elif j:
+                    j=fails[j-1]
+                else:
+                    fails.append(0)
+                    i+=1
+                    yield
+        cfails = addfails()
         j=0
         i=len(s)-1
         while i>j:
-            k=fails[-1]
-            while j>=len(fails):
-                if s[k] == s[j]:
-                    fails.append(fails[k]+1)
-                elif k>0:
-                    k=fails[k-1]
-                else:
-                    fails.append(0)
+            if j>=len(fails):
+                next(cfails)
             while True:
                 if s[i]==s[j]:
-                    j+=1
                     i-=1
+                    j+=1
                     break
-                elif j>0:
+                elif j:
                     j=fails[j-1]
                 else:
                     i-=1
                     break
         if i==j-1:
-            l = j*2
+            l=j*2
         else:
-            l = i*2 + 1
+            l=i*2+1
         return s[l:][::-1] + s
-                    
-        
