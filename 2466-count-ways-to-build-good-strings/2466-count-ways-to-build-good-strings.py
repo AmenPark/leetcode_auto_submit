0cdf477a35d43c66ce285fb7adf3adfdc851a071
Amen Park <86364207+AmenPark@users.noreply.github.com>
2025-04-19 18:23:36 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        dp = {0:1}
+        ans = 0
+        i=0
+        while i<=high:
+            if i not in dp:
+                i+=1
+                continue
+            dp[i+one] = dp.get(i+one,0)+ dp[i]
+            dp[i+zero] = dp.get(i+zero,0)+ dp[i]
+            if i>=low:
+                ans+=dp[i]
+            i+=1
+        print(dp)
+        return ans
